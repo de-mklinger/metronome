dca8a899e01772efb13a0e69fa5ebdfb1d4233b3
Marc Klinger <mklinger@mklinger.de>
2021-05-07 16:08:52 +0200

    Mock function for test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
-import { ctx } from '../index.testFixtures';
+import {ctx} from '../index.testFixtures';
 
 test('renders tap', () => {
-  render(<App ctx={ctx} />);
-  const linkElement = screen.getByText(/Tap/i);
-  expect(linkElement).toBeInTheDocument();
+    // https://github.com/jsdom/jsdom/issues/1695
+    if (!window.HTMLElement.prototype.scrollIntoView) {
+        window.HTMLElement.prototype.scrollIntoView = () => {
+        };
+    }
+
+    render(<App ctx={ctx}/>);
+    const linkElement = screen.getByText(/Tap/i);
+    expect(linkElement).toBeInTheDocument();
 });
