3ddffc88cb59675392c74f839fda32369164bcf0
Marc Klinger <mklinger@mklinger.de>
2024-06-01 15:28:00 +0200

    Apply prettier

--- src/components/setlist/SetlistEditView.tsx
+++ src/components/setlist/SetlistEditView.tsx
@@ -1,48 +1,57 @@
 import SetlistEditorSongs from "./SetlistEditorSongs.js";
-import {NewSetlistWithSongs, Song} from "../../types.ts";
+import { NewSetlistWithSongs, Song } from "../../types.ts";
 
 export type SetlistEditViewProps<T extends NewSetlistWithSongs> = {
-    setlist: T,
-    onChange: (setlist: T) => void
-}
+  setlist: T;
+  onChange: (setlist: T) => void;
+};
 
-function SetlistEditView<T extends NewSetlistWithSongs>({setlist, onChange}: SetlistEditViewProps<T>) {
-    function fireChange<K extends keyof T>(attributeName: K, attributeValue: T[K]) {
-        const changedSetlist = { ...setlist };
-        changedSetlist[attributeName] = attributeValue;
-        onChange(changedSetlist);
-    }
+function SetlistEditView<T extends NewSetlistWithSongs>({
+  setlist,
+  onChange,
+}: SetlistEditViewProps<T>) {
+  function fireChange<K extends keyof T>(
+    attributeName: K,
+    attributeValue: T[K],
+  ) {
+    const changedSetlist = { ...setlist };
+    changedSetlist[attributeName] = attributeValue;
+    onChange(changedSetlist);
+  }
 
-    const fireSongsChange = (songs: Song[]) => {
-        const changedSetlist = { ...setlist };
-        changedSetlist.songs = songs;
-        changedSetlist.songIds = songs.map(song => song.id);
-        onChange(changedSetlist);
-    }
+  const fireSongsChange = (songs: Song[]) => {
+    const changedSetlist = { ...setlist };
+    changedSetlist.songs = songs;
+    changedSetlist.songIds = songs.map((song) => song.id);
+    onChange(changedSetlist);
+  };
 
-    return (
-            <>
-                <div className="form-group">
-                    <label htmlFor="title">Title</label>
-                    <input type="text" className="form-control" id="title" placeholder="Enter setlist title"
-                           value={setlist.title}
-                           autoFocus={true}
-                           onChange={e => fireChange("title", e.target.value)}
-                    />
-                </div>
+  return (
+    <>
+      <div className="form-group">
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          className="form-control"
+          id="title"
+          placeholder="Enter setlist title"
+          value={setlist.title}
+          autoFocus={true}
+          onChange={(e) => fireChange("title", e.target.value)}
+        />
+      </div>
 
-                {
-                    setlist.songs && setlist.songs.length > 0 &&
-                    <div className="form-group">
-                        <label>Songs</label>
-                        <SetlistEditorSongs
-                            songs={setlist.songs}
-                            onSongsChange={fireSongsChange}
-                        />
-                    </div>
-                }
-            </>
-    );
+      {setlist.songs && setlist.songs.length > 0 && (
+        <div className="form-group">
+          <label>Songs</label>
+          <SetlistEditorSongs
+            songs={setlist.songs}
+            onSongsChange={fireSongsChange}
+          />
+        </div>
+      )}
+    </>
+  );
 }
 
 export default SetlistEditView;
--- src/components/song/SongEditor.tsx
+++ src/components/song/SongEditor.tsx
@@ -1,86 +1,108 @@
 import BpmKnob from "../common/BpmKnob.tsx";
 import Accents from "./Accents.js";
 import TimeSignature from "./TimeSignature.js";
-import {NewSong} from "../../types.ts";
+import { NewSong } from "../../types.ts";
 import Row from "../controls/Row.tsx";
 import Col from "../controls/Col.tsx";
-import {FormattedMessage} from "react-intl";
+import { FormattedMessage } from "react-intl";
 
 export type SongEditorProps<T extends NewSong> = {
-  song: T
-  onChange: (song: T) => void
-}
+  song: T;
+  onChange: (song: T) => void;
+};
 
-export default function SongEditor<T extends NewSong>({song, onChange}: SongEditorProps<T>) {
-    function fireChange<K extends keyof T>(attributeName: K, attributeValue: T[K]) {
-        const changedSong = { ...song };
-        changedSong[attributeName] = attributeValue;
-        onChange(changedSong);
-    }
+export default function SongEditor<T extends NewSong>({
+  song,
+  onChange,
+}: SongEditorProps<T>) {
+  function fireChange<K extends keyof T>(
+    attributeName: K,
+    attributeValue: T[K],
+  ) {
+    const changedSong = { ...song };
+    changedSong[attributeName] = attributeValue;
+    onChange(changedSong);
+  }
 
-    return (
-        <>
-            <div className="form-group">
-                <label htmlFor="title">
-                  <FormattedMessage id="song.title" />
-                </label>
-                <input type="text" className="form-control" id="title" placeholder="Enter song title"
-                       value={song.title}
-                       autoFocus={true}
-                       onChange={e => fireChange("title", e.target.value)}
-                />
-            </div>
-            <div className="form-group">
-                <Row>
-                    <Col>
-                        <label htmlFor="bpm">
-                          <FormattedMessage id="song.bpm" />
-                        </label>
-                        <input type="number" className="form-control" id="bpm" placeholder="Enter BPM"
-                               min={1}
-                               value={song.bpm}
-                               onChange={e => fireChange("bpm", parseInt(e.target.value, 10))}
-                        />
-                    </Col>
-                    <Col className="speed-knob-col">
-                        <BpmKnob
-                            bpm={song.bpm}
-                            onBpmChange={bpm => fireChange("bpm", bpm)}
-                        />
-                    </Col>
-                </Row>
-            </div>
-            <div className="form-group">
-                <label htmlFor="timeSignatureBeats">
-                  <FormattedMessage id="song.time-signature"/>
-                </label>
-                <TimeSignature
-                    beats={song.timeSignatureBeats}
-                    noteValue={song.timeSignatureNoteValue}
-                    onBeatsChange={beats => fireChange("timeSignatureBeats", beats)}
-                    onNoteValueChange={noteValue => fireChange("timeSignatureNoteValue", noteValue)}
-                />
-            </div>
-            <div className="form-group">
-                <label htmlFor="subDivisions">
-                  <FormattedMessage id="song.sub-divisions" />
-                </label>
-                <input type="number" className="form-control" id="subDivisions" placeholder="Enter subDivisions"
-                       value={song.subDivisions}
-                       onChange={e => fireChange("subDivisions", parseInt(e.target.value, 10))}
-                />
-            </div>
+  return (
+    <>
+      <div className="form-group">
+        <label htmlFor="title">
+          <FormattedMessage id="song.title" />
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="title"
+          placeholder="Enter song title"
+          value={song.title}
+          autoFocus={true}
+          onChange={(e) => fireChange("title", e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <Row>
+          <Col>
+            <label htmlFor="bpm">
+              <FormattedMessage id="song.bpm" />
+            </label>
+            <input
+              type="number"
+              className="form-control"
+              id="bpm"
+              placeholder="Enter BPM"
+              min={1}
+              value={song.bpm}
+              onChange={(e) => fireChange("bpm", parseInt(e.target.value, 10))}
+            />
+          </Col>
+          <Col className="speed-knob-col">
+            <BpmKnob
+              bpm={song.bpm}
+              onBpmChange={(bpm) => fireChange("bpm", bpm)}
+            />
+          </Col>
+        </Row>
+      </div>
+      <div className="form-group">
+        <label htmlFor="timeSignatureBeats">
+          <FormattedMessage id="song.time-signature" />
+        </label>
+        <TimeSignature
+          beats={song.timeSignatureBeats}
+          noteValue={song.timeSignatureNoteValue}
+          onBeatsChange={(beats) => fireChange("timeSignatureBeats", beats)}
+          onNoteValueChange={(noteValue) =>
+            fireChange("timeSignatureNoteValue", noteValue)
+          }
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="subDivisions">
+          <FormattedMessage id="song.sub-divisions" />
+        </label>
+        <input
+          type="number"
+          className="form-control"
+          id="subDivisions"
+          placeholder="Enter subDivisions"
+          value={song.subDivisions}
+          onChange={(e) =>
+            fireChange("subDivisions", parseInt(e.target.value, 10))
+          }
+        />
+      </div>
 
-            <div className="form-group">
-                <label htmlFor="accent0">
-                  <FormattedMessage id="song.accents"/>
-                </label>
-                <Accents
-                    timeSignatureBeats={song.timeSignatureBeats}
-                    accents={song.accents}
-                    onAccentsChange={accents => fireChange("accents", accents)}
-                />
-            </div>
-        </>
-    );
+      <div className="form-group">
+        <label htmlFor="accent0">
+          <FormattedMessage id="song.accents" />
+        </label>
+        <Accents
+          timeSignatureBeats={song.timeSignatureBeats}
+          accents={song.accents}
+          onAccentsChange={(accents) => fireChange("accents", accents)}
+        />
+      </div>
+    </>
+  );
 }
