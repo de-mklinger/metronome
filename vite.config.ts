ded7add6acde796625295e5c61aec41ae5b6c7b5
Marc Klinger <mklinger@mklinger.de>
2024-09-29 21:48:18 +0200

    Use generated manifest and service worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,52 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/metronome/",
   plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Metronome",
+        short_name: "Metronome",
+        icons: [
+          {
+            src: "logo-maskable-bg-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "logo-maskable-bg-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "logo-maskable-bg.svg",
+            type: "image/svg+xml",
+          },
+          {
+            src: "logo.svg",
+            type: "image/svg+xml",
+            purpose: "monochrome",
+          },
+        ],
+        theme_color: "#333333",
+        background_color: "#333333",
+        display: "standalone",
+      },
+    }),
     svgr(),
-    react()
+    react(),
   ],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
     // css: true,
   },
-})
+});
